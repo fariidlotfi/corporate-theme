183b724d05fdd617b32fe929412e506a1b6a87fb
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2024-02-02 18:17:27 +0330

    fix: images

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,15 +2,15 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import UpperNavBar from "./UpperNavBar";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 
 export default function NavBar() {
-const [menuHeight, setMenuHeight] = useState("0")
+  const [menuHeight, setMenuHeight] = useState("0");
 
   const menuExpander = () => {
     menuHeight == "0" ? setMenuHeight("250px") : setMenuHeight("0");
-  }
+  };
 
   return (
     <header>
@@ -20,11 +20,11 @@ const [menuHeight, setMenuHeight] = useState("0")
 
       <div className="main-header">
         <div className="logo">
-          <img src="src/assets/logo.png" />
+          <img src="assets/logo.png" />
         </div>
 
-        <MenuIcon className="menu-icon-header" onClick={menuExpander}/>
-        <nav className="navbar-nav-expander" style={{height: menuHeight }}>
+        <MenuIcon className="menu-icon-header" onClick={menuExpander} />
+        <nav className="navbar-nav-expander" style={{ height: menuHeight }}>
           <ul>
             <li>
               <Link to="/">صفحه اصلی</Link>
